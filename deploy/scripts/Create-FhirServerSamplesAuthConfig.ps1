e3f62cc49e7ae649eccdcdc3be8d62c9215da681
David Beck <frozen3@aol.com>
2021-11-22 12:25:14 -0600

    commercial+localdomain set

--- deploy/scripts/Create-FhirServerSamplesAuthConfig.ps1
+++ deploy/scripts/Create-FhirServerSamplesAuthConfig.ps1
@@ -24,7 +24,7 @@ param
     [string]$EnvironmentLocation = "West US",
 
     [Parameter(Mandatory = $false )]
-    [String]$WebAppSuffix = "azurewebsites.us",
+    [String]$WebAppSuffix = "azurewebsites.com",
 
     [Parameter(Mandatory = $false)]
     [string]$ResourceGroupName = $EnvironmentName,
@@ -130,7 +130,7 @@ Write-Host "Ensuring API application exists"
 
 $fhirServiceName = "${EnvironmentName}srvr"
 if ($UsePaas) {
-    $fhirServiceUrl = "https://${EnvironmentName}.azurehealthcareapis.us"
+    $fhirServiceUrl = "https://${EnvironmentName}.frozen3aol.onmicrosoft.com"
 } else {
     $fhirServiceUrl = "https://${fhirServiceName}.${WebAppSuffix}"    
 }
